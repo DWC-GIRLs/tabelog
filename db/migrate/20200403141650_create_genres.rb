1221584693250f67abc7b403902ecbfdaac1fe3f
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-03 14:17:52 +0000

    Add: ジャンルモデル作成

---
+++ db/migrate/20200403141650_create_genres.rb
@@ -0,0 +1,10 @@
+class CreateGenres < ActiveRecord::Migration[5.2]
+  def change
+    create_table :genres do |t|
+      t.string :code
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
