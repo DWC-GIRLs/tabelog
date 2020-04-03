0d150e5f2a1578124d6630abeb84dd8a201e6f25
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-03 14:40:56 +0000

    Add: レビュー画像テーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_143142) do
+ActiveRecord::Schema.define(version: 2020_04_03_143907) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -64,6 +64,13 @@ ActiveRecord::Schema.define(version: 2020_04_03_143142) do
 
   create_table "restaurant_images", force: :cascade do |t|
     t.integer "restaurant_id", null: false
+    t.string "image", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "review_images", force: :cascade do |t|
+    t.integer "review_id"
     t.string "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
