677f2242347a2e00e3b6a931c2d99da39b577dbf
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-03 14:07:27 +0000

    Add: ownersテーブルにカラムを追加

--- db/migrate/20200403134303_devise_create_owners.rb
+++ db/migrate/20200403134303_devise_create_owners.rb
@@ -32,7 +32,15 @@ class DeviseCreateOwners < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.integer :status, default: 0, null: false
+      t.string :first_name
+      t.string :last_name
+      t.string :first_kana
+      t.string :last_kana
+      t.string :nick_name
+      t.integer :phone_number
+      t.integer :password
+      
       t.timestamps null: false
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_130841) do
+ActiveRecord::Schema.define(version: 2020_04_03_134303) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,6 +33,26 @@ ActiveRecord::Schema.define(version: 2020_04_03_130841) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "owners", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "status", default: 0, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "first_kana"
+    t.string "last_kana"
+    t.string "nick_name"
+    t.integer "phone_number"
+    t.integer "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_owners_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
   end
 
 end
