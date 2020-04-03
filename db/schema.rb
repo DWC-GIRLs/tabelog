9dd220855f026bbfcee78fbe49e07aa4bdeb7c38
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-03 14:25:27 +0000

    Add: カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_141650) do
+ActiveRecord::Schema.define(version: 2020_04_03_142130) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -60,6 +60,13 @@ ActiveRecord::Schema.define(version: 2020_04_03_141650) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_owners_on_email", unique: true
     t.index ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
+  end
+
+  create_table "restaurant_images", force: :cascade do |t|
+    t.integer "restaurant_id", null: false
+    t.string "image", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
