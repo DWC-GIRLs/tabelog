109bd20e18579efdb0055bb0ee79aad760c02d90
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-04 04:49:53 +0000

    Add: レビューテーブルにレストランidを追加

---
+++ db/migrate/20200404042010_add_details_to_reviews.rb
@@ -0,0 +1,5 @@
+class AddDetailsToReviews < ActiveRecord::Migration[5.2]
+  def change
+    add_column :reviews, :restaurant_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_040952) do
+ActiveRecord::Schema.define(version: 2020_04_04_042010) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -161,6 +161,7 @@ ActiveRecord::Schema.define(version: 2020_04_04_040952) do
     t.integer "meal_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "restaurant_id"
   end
 
 end
