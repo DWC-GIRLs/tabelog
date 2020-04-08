fd4a1e62ad2a88019352fd7829fe3262a861a51f
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-08 09:17:08 +0000

    Add: ログイン、ログアウト後の画面遷移を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  #ログイン後の画面遷移
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+  #ログアウト後の画面遷移
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
