17dfbb117b6b686398e0fbe3e3895520d589611c
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-08 15:56:59 +0000

    Change: アカウントのルーティングをscop moduleに変更してurlに表示、ルーティングに影響がないようにしたよう

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,26 @@
 Rails.application.routes.draw do
+  devise_for :accounts, controllers: { omniauth_callbacks: 'accounts/omniauth_callbacks' }
   devise_for :owners
-  devise_for :accounts
 
   ######## ここからアカウント ########
-  namespace :accounts do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :accounts do
+  scope module: :accounts do
+    # homes
+    root 'homes#top'
+    get 'help/beginner/' => 'homes#about', as: :about
+
     get 'search/area'
     get 'search/keyword'
     get 'search/genre'
     get 'search/booking'
     get 'search/detail'
-  end
-  namespace :accounts do
+
     get 'restaurants/show'
     get 'restaurants/index'
-  end
-  namespace :accounts do
+
     get 'booking_histories/index'
     get 'booking_histories/new'
     get 'booking_histories/create'
-  end
-  namespace :accounts do
+
     get 'reviews/index'
     get 'reviews/new'
     get 'reviews/create'
@@ -32,8 +29,7 @@ Rails.application.routes.draw do
     get 'reviews/destroy'
     get 'reviews/rate_create'
     get 'reviews/rate_update'
-  end
-  namespace :accounts do
+
     get 'accounts/mypage'
     get 'accounts/edit'
     get 'accounts/update'
@@ -42,6 +38,8 @@ Rails.application.routes.draw do
     get 'accounts/welcome'
     get 'accounts/confirm'
     get 'accounts/goodbye'
+    get 'accounts/login' => 'devise/sessions#new'
+    get 'accounts/member_entry' => 'registrations#new'
   end
 
 
@@ -50,8 +48,7 @@ Rails.application.routes.draw do
     get 'booking_histories/show'
     get 'booking_histories/index'
     get 'booking_histories/update'
-  end
-  namespace :owners do
+
     get 'restaurants/mypage'
     get 'restaurants/new'
     get 'restaurants/create'
@@ -61,8 +58,7 @@ Rails.application.routes.draw do
     get 'restaurants/confirm'
     get 'restaurants/goodbye'
     get 'restaurants/destroy'
-  end
-  namespace :owners do
+
     get 'owners/mypage'
     get 'owners/edit'
     get 'owners/update'
