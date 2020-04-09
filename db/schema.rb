fbbe2a491a1fa5ff76e61c688dbdcab8df2ba7f2
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-09 10:37:24 +0000

    Add: accountsテーブルにproviderとuidの組み合わせユーザーが一意に定まるようにUNIQUEインデックスを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_062705) do
+ActiveRecord::Schema.define(version: 2020_04_09_103047) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_04_06_062705) do
     t.string "provider"
     t.string "uid"
     t.index ["email"], name: "index_accounts_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_accounts_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
 
