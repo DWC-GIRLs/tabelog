42019e6fd0f2a6851a26e66ea3daed3eac3c60d6
kanon2580 <kanon.ueno1117@gmail.com>
2020-04-10 12:58:10 +0900

    [update]setting attachment

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,8 @@
 class Restaurant < ApplicationRecord
-
   has_many :booking_histries, dependent: :destroy
   has_many :rates,            dependent: :destroy
   has_many :reviews,          dependent: :destroy
-  has_many :review_images     dependent: :destroy
-  
+  has_many :review_images,    dependent: :destroy
+
+  attachment :logo_image  
 end
--- app/models/restaurant_image.rb
+++ app/models/restaurant_image.rb
@@ -1,5 +1,5 @@
 class RestaurantImage < ApplicationRecord
-
   belongs_to :restaurant
-  
+
+  attachment :image  
 end
