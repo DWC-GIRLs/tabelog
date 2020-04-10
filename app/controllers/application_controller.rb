be5f1531cd2b770276ff759ceca904fd4fd2ac40
kanon2580 <kanon.ueno1117@gmail.com>
2020-04-10 11:46:35 +0900

    [add]owner新規登録フォーム、ログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,4 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     new_owner_session_path
   end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:status, :first_name, :last_name, :first_kana, :last_kana, :nick_name, :phone_number, :password, :password_confirmation])
-  end  
 end
