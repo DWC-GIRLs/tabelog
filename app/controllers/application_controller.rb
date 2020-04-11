aec6052c280970ca493403e6e7777d419cb2a1ac
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-11 13:24:06 +0000

    Add: アカウント新規登録にニックネーム追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   #ログイン後の画面遷移
   def after_sign_in_path_for(resource)
     root_path
@@ -7,4 +9,12 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:nick_name, :email, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+  end
+
 end
