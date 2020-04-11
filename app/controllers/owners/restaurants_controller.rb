6ed98483de75ed7fdb61c01ebe34e7f9f8341772
kanon2580 <kanon.ueno1117@gmail.com>
2020-04-11 20:06:07 +0900

    [add]new restaurant form etc

--- app/controllers/owners/restaurants_controller.rb
+++ app/controllers/owners/restaurants_controller.rb
@@ -1,11 +1,20 @@
 class Owners::RestaurantsController < ApplicationController
-  def mypage
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
+    @new_restaurant = Restaurant.new
   end
 
   def create
+    new_restaurant = Restaurant.new(restaurant_params)
+    if new_restaurant.save
+      redirect_to owners_restaurant_path(new_restaurant)
+    else
+      flash[:error] = "保存されてませ～んm9(^Д^)ﾌﾟｷﾞｬｰ"
+      redirect_back(fallback_location: owners_mypage_path(current_owner))
+    end
   end
 
   def welcome
@@ -24,5 +33,19 @@ class Owners::RestaurantsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:shop_id, :name, :logo_image, :name_kana, :address,
+                                        :station_name, :l_area_code, :m_area_code, :s_area_code,
+                                        :lat, :lng, :genre_code, :genre_name, :search_keyword,
+                                        :catch, :capacity, :access, :urls, :photo, :open, :close,
+                                        :party_capacity, :wifi, :free_drink, :free_food, :private_room,
+                                        :horigotatsu, :tatami, :card, :non_smoking, :charter, :ktai,
+                                        :parking, :barrier_free, :other_memo, :karaoke, :tv, :english,
+                                        :pet, :child, :lunch, :midnight, :shop_detail_memo, :coupon_urls,
+                                        restaurant_images_images: []
+                                      )
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,8 @@ class Restaurant < ApplicationRecord
   has_many :booking_histries, dependent: :destroy
   has_many :rates,            dependent: :destroy
   has_many :reviews,          dependent: :destroy
-  has_many :review_images,    dependent: :destroy
+  has_many :restaurant_images,    dependent: :destroy
 
   attachment :logo_image
+  accepts_attachments_for :restaurant_images, attachment: :image
 end
