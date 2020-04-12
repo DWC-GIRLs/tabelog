a32901bc5ecc9435ab80819700d66f10e2f02de5
saeko9741 <saeco9741@gmail.com>
2020-04-12 02:42:10 +0000

    Add: restaurant api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'net/http'
+require 'json'
+require 'pry'
+require 'dotenv'
+Dotenv.load
+
+uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{ENV['RESTAURANT_API_KEY']}&count=2&format=json&keyword=%E9%96%A2%E6%9D%B1")
+response = Net::HTTP.get(uri)
+parsed_response = JSON.parse(response)
+shops = parsed_response["results"]["shop"]
+shops.each do |shop|
+  Restaurant.create(
+    shop_id: shop['id'],
+    name: shop['name']
+  )
+end
