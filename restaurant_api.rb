4b5846a043ffbc40edb990509fa619144f0349d5
saeko9741 <saeco9741@gmail.com>
2020-04-12 02:41:10 +0000

    Update: restaurant api added loop

--- restaurant_api.rb
+++ restaurant_api.rb
@@ -4,10 +4,13 @@ require 'pry'
 require 'dotenv'
 Dotenv.load
 
-uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{ENV['RESTAURANT_API_KEY']}&count=2&format=json&keyword=%E6%96%B0%E5%AE%BF%0A")
+uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{ENV['RESTAURANT_API_KEY']}&count=2&format=json&keyword=%E9%96%A2%E6%9D%B1")
 response = Net::HTTP.get(uri)
 parsed_response = JSON.parse(response)
 shops = parsed_response["results"]["shop"]
-# shop_ids = shops.map {|shop| shop["id"]}
-# shop_names = shops.map {|shop| shop["name"]}
-puts parsed_response
+shops.each do |shop|
+  Restaurant.create(
+    shop_id: shop['id'],
+    name: shop['name']
+  )
+end
