fc1b62ad6723b85b5a820e93068fee403f4b7b43
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-13 16:00:34 +0000

    Change: gem変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_04_09_103047) do
-  
+
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
