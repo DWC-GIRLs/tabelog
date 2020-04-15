71f9e8d5958a1db451d5ab6862e9c07cbca11182
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-15 12:43:58 +0000

    Add: facebook認証からニックネームを入力して新規登録できるようにした

--- app/controllers/accounts/omniauth_callbacks_controller.rb
+++ app/controllers/accounts/omniauth_callbacks_controller.rb
@@ -7,9 +7,9 @@ class Accounts::OmniauthCallbacksController < Devise::OmniauthCallbacksControlle
     if account.persisted? #DBに保存済みか（オブジェクトが新しくない&&削除されてない場合にtrue）
       sign_in_and_redirect account, event: :authentication #サインイン後、保存された場所にリダイレクトしてから、after_sign_in_path_forで指定されたURLにリダイレクト
       set_flash_message(:notice, :success, kind: "#{account.provider}".capitalize) if is_navigational_format? #.capitalize(先頭のみ大文字)
-    else # transaction発火したら
-      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra") #extraは除く
-      redirect_to new_user_registration_url
+    else #unless側を通ったレスポンス
+      session["devise.#{account.provider}_data"] = account
+      redirect_to new_account_registration_url
     end
   end
 
