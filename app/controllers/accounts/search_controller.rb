1e8cfcf228014b9a9806f02953ccad914da4bb58
kanon2580 <kanon.ueno1117@gmail.com>
2020-04-27 16:46:20 +0900

    [add]area leyword search

--- app/controllers/accounts/search_controller.rb
+++ app/controllers/accounts/search_controller.rb
@@ -5,7 +5,13 @@ class Accounts::SearchController < ApplicationController
   end
 
   def keyword
-    @restaurants = Restaurant.search_by_keyword(params[:q])
+    s_area_codes = fetch_s_area_codes(params[:area_q])
+    results_by_area_codes = Restaurant.search_by_area_codes(s_area_codes) unless s_area_codes
+
+    results_by_restaurant_address = Restaurant.search_by_restaurant_address(params[:area_q])
+
+    results = (results_by_area_codes += results_by_restaurant_address).flatten.uniq
+    @restaurants= results
     render template: "accounts/restaurants/index"
   end
 
@@ -17,5 +23,18 @@ class Accounts::SearchController < ApplicationController
   end
 
   def detail
+  end
+
+  private
+  def fetch_s_area_codes(area_q)
+    encoded_uri = URI.encode("https://webservice.recruit.co.jp/hotpepper/small_area/v1/?key=#{ENV['RESTAURANT_API_KEY']}&keyword=#{area_q}&format=json")
+    patsed_uri = URI.parse(encoded_uri)
+    response = Net::HTTP.get(patsed_uri)
+    results = JSON.parse(response)['results']
+
+    return false if results['results_available'] == 0
+
+    area_codes = results["#{area_size}_area"].map{|result| result['code']}
+    return area_codes
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,41 +7,21 @@ class Restaurant < ApplicationRecord
   attachment :logo_image
   accepts_attachments_for :restaurant_images, attachment: :image
 
-  def self.search_by_keyword(q)
-    require 'net/http'
-    require 'json'
-    Dotenv.load
-
-    splited_q = q.split(/[[:blank:]]+/)
-    restaurants = []
-    binding.pry
-    splited_q.each do |q|
-      next if q == ""
-      # queryに地名が含まれていたらその情報を優先して出す
-      l_area_response = Net::HTTP.get("http://webservice.recruit.co.jp/hotpepper/large_area/v1/?key=#{ENV['RESTAURANT_API_KEY']}&keyword=#{q}")
-      parsed_l_area_response = JSON.parse(l_area_response)
-      if l_area_response['results']['results_available'] === 0
-        search_db(q)
-      else
-        Restaurant.find_by(l_area_code: ['results']['large_area']['code'])
-      end
-    end
-    return restaurants.uniq!
-  end
-
-  def search_db(query)
-    query.each do |q|
-      restaurants += Restaurant.where('name LIKE ?', "%#{q}%")
-      restaurants += Restaurant.where('name_kana LIKE ?', "%#{q}%")
-      restaurants += Restaurant.where('search_keyword LIKE ?', "%#{q}%")
-      restaurants += Restaurant.where('catch LIKE ?', "%#{q}%")
+  def self.search_by_area_codes(area_codes)
+    area_resutlts = area_codes.map{|code| Restaurant.where(s_area_code: code)}
+    if area_resutlts.size == 0
+      return false
+    else
+      return area_results
     end
   end
 
-  def self.search_area(area)
-    return Restaurant.all unless area
-    Restaurant.where(['address LIKE ?', "%#{area}%"])
-    #文字列のどの部分にでも検索したい文字列が含まれていればOK
+  def self.search_by_restaurant_address(area_q)
+    phrase_results = Restaurant.where(['address LIKE ?', "%#{area_q}%"])
+    splited_area_q = area_q.split(/\s*/)
+    broad_results = splited_area_q.map{|q| Restaurant.where(['address LIKE ?', "%#{q}%"])}
+    results = (phrase_results += broad_results).flatten.uniq
+    return results
   end
 
   def self.search_genre(genre)
