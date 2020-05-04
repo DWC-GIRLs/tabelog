36fe28a34d17dd6e6d4b6772d19c66381e256137
saeko9741 <saeco9741@gmail.com>
2020-05-04 04:10:46 +0000

    Add: restaurant name, time on the modal form

--- app/controllers/accounts/booking_histories_controller.rb
+++ app/controllers/accounts/booking_histories_controller.rb
@@ -3,10 +3,31 @@ class Accounts::BookingHistoriesController < ApplicationController
   end
 
   def new
+    # @booking_history = BookingHistory.new
+    @restaurant_id = params[:restaurant_id]
+    @restaurant_name = params[:restaurant_name]
+    @date = params[:date]
+    @time = params[:time]
+    @people_count = params[:people_count]
   end
 
   def create
     @booiking_history = BookingHistory.new(booiking_history)
+
+
+    respond_to do |format|
+      if @booking_history.save
+        format.html { redirect_to @booking_history, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @booking_history }
+        # 追加
+        format.js { @status = "success" }
+      else
+        format.html { render :new }
+        format.json { render json: @booking_history.errors, status: :unprocessable_entity }
+        # 追加
+        format.js { @status = "fail" }
+      end
+    end
   end
 
   private
